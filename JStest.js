89709b423399de59b1890f9860ea718b9dfa288a
Xiechen233 <50294080+Xiechen233@users.noreply.github.com>
2020-07-01 10:17:55 +0800

    Add files via upload

---
+++ JStest.js
@@ -0,0 +1,10 @@
+var CANVAS_WIDTH=800,CANVAS_HEIGHT=2000;
+window.onload=function(){
+    createCanvas();
+}
+function createCanvas(){
+    document.body.innerHTML="<canvas id=\"mycanvas\" width=\""+CANVAS_WIDTH+"\" height=\""+CANVAS_HEIGHT+"\"></canvas>"
+}
+function drawRect(){
+    
+}
